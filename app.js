0aa9dae7681bf5594c6f91048f138501bbd3ac88
karljorgent <karl-jorgen.tormet@khk.ee>
2021-09-24 11:17:42 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("test");
+// 1 rida
+
+/*
+mitu
+rida
+*/
+
+//muutuja mille sisu saab muuta
+let name = 'Jörgen';
+
+name = 'Bob'
+
+//muutuja, mis on konstantne
+const surName = 'Tamm'
+
+//massiivid
+
+const numbers = [1, 2, 3, 4]
+numbers.push(6)
+
+// objektid
+const person = {
+	name: 'Kadi',
+	age: 20,
+}
+person.age = 21
+
+console.log(person)
