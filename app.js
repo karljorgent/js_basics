20f1e87c733d1f3572e52d9e9f82a65383e0bb2e
karljorgent <karl-jorgen.tormet@khk.ee>
2021-09-27 13:43:47 +0300

    Objects

--- app.js
+++ app.js
@@ -1,47 +1,26 @@
-const numbers = [43, 56, 33, 23, 44, 36, 5];
-const numbers2 = new Array(22, 43, 64, 13);
-const fruits = ['õun', 'banaan', 'pirn'];
-const mixed = [13, 'Bob', true, undefined, null, {a:1, b:2}, new Date()]
+const person = {
+	firstname: 'Kadi',
+	lastname: 'Tamm',
+	age: '23',
+	email: 'kadi.tamm@gmail.com',
+	hobbies: ['muusika', 'sport'],
+	address: {
+		city: 'Tallinn',
+		county: 'Harjumaa'
+	},
+	getBirthdayYear: function(){
+		return 2021 - this.age;
+	}
+};
 
 let val;
 
-val = numbers.length; //masiivi pikkus
-val = Array.isArray(numbers); //kas on masiiv?
-val = numbers[3]; // kindel väärtus
+val = person.firstname;
+val = person['lastname'];
+val = person.age
+val = person.hobbies[1];
+val = person.address.city;
+val = person.getBirthdayYear()
 
-//numbers[3] = 100 //asendamine
 
-// lisamine
-//numbers.push(250);
-//numbers.unshift(120);
-
-//maha võtmine
-numbers.pop();
-numbers.shift();
-
-//index
-//val = numbers.indexOf(100);
-
-//lõikamine
-//numbers.splice(1, 2);
-
-// ühendamine
-val = numbers.concat(numbers2);
-
-//sorteerimine
-val = fruits.sort();
-
-//kasvujärjekorras
-val = numbers.sort(function(x, y){
-	return x-y
-});
-
-//kahanevasjärjekorras
-val = numbers.sort(function(x, y){
-	return y-x
-});
-
-numbers.splice(3, false, 100)
-
-console.log(numbers);
-console.log(val);
+console.log(val)
