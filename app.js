cbe727b88b5634ac2f98d8d966fcb7339548ac16
karljorgent <karl-jorgen.tormet@khk.ee>
2021-09-27 15:32:34 +0300

    Tsükkel

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-let color = 'red'
-
-switch(color){
-	case 'red':
-		console.log('Stop!');
-		break;
-	case 'yellow':
-		console.log('Attention!');
-		break;
-	case 'green':
-		console.log('GO!');
-		break;
-	default:
-		console.log('This color is not allowed!');
-		break;
+// dor tsükkel
+for(let i = 0; i < 10; i++) {
+	//if(i % 2 == 0)
+		//console.log(i);
 }
 
-date = new Date();
-weekDay = date.getDay();
-let day;
+for(let i = 0; i < 10; i++) {
+		// if (i === 5) break;
+		if(i === 5) continue;
+		console.log(i);
+}
 
-if(weekDay === 1) day = 'Esmaspäev';
-else if(weekDay === 2) day = 'Teisipäev';
-else if(weekDay === 3) day = 'Kolmapäev';
-else if(weekDay === 4) day = 'Neiljapäev';
-else if(weekDay === 5) day = 'Reede';
-else if(weekDay === 6) day = 'Laupäev';
-else if(weekDay === 0) day = 'Pühapäev';
-else console.log('Miskit lappes ei vä?')
-console.log(day)
+// while
+let i = 0
+while(i < 20) {
+	console.log(i)
+	i++;
+}
+
+// do while
+let j = 0;
+do{
+	console.log(j);
+	j++;
+} while(j < 10);
+
+// tsükklid ja massivid
+const cars = ['Ford', 'Honda', 'Toyota'];
+//for
+for(let i = 0; i < cars.length; i++){
+	console.log(cars[i]);
+}
+//forEach()
+cars.forEach(function(car, index){
+	console.log(`${index} - ${car}`);
+})
+
+const person = {
+	firstname: 'Kadi',
+	lastname: 'Tamm',
+	age: 25
+};
+for(let element in person){
+	console.log(`${element} - ${person[element]}`)
+}
