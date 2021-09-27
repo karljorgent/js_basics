51834bbf3510becdc27c57319ab6a03c1c8e7aa0
karljorgent <karl-jorgen.tormet@khk.ee>
2021-09-27 13:27:09 +0300

    Arrays and methods

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
-const firstname = 'Kadi';
-const surname = 'Tamm';
-const age = 20;
-const tags = 'arendus,veeb';
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22, 43, 64, 13);
+const fruits = ['õun', 'banaan', 'pirn'];
+const mixed = [13, 'Bob', true, undefined, null, {a:1, b:2}, new Date()]
 
 let val;
 
-// templating - es6
-val = `
-	<ul>
-		<li>Eesnimi: ${firstname}</li>
-		<li>Perenimi: ${surname}</li>
-		<li>Vanus: ${age}</li>
-		<li>Alad: ${tags}</li>
-</ul>
-`;
+val = numbers.length; //masiivi pikkus
+val = Array.isArray(numbers); //kas on masiiv?
+val = numbers[3]; // kindel väärtus
 
+//numbers[3] = 100 //asendamine
+
+// lisamine
+//numbers.push(250);
+//numbers.unshift(120);
+
+//maha võtmine
+numbers.pop();
+numbers.shift();
+
+//index
+//val = numbers.indexOf(100);
+
+//lõikamine
+//numbers.splice(1, 2);
+
+// ühendamine
+val = numbers.concat(numbers2);
+
+//sorteerimine
+val = fruits.sort();
+
+//kasvujärjekorras
+val = numbers.sort(function(x, y){
+	return x-y
+});
+
+//kahanevasjärjekorras
+val = numbers.sort(function(x, y){
+	return y-x
+});
+
+numbers.splice(3, false, 100)
+
+console.log(numbers);
 console.log(val);
