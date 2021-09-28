eba033da720344ec6a5cc512af67c9ecd15d392f
karljorgent <karl-jorgen.tormet@khk.ee>
2021-09-28 15:40:35 +0300

    DOM - Document Object Modle

--- app.js
+++ app.js
@@ -1,48 +1,30 @@
-// deklareerimine
-function greeting(firstname = 'Kadi', lastname = 'Tamm'){
-	/*
-	if(typeof firstname === 'undefined') {
-		firstname = 'Kadi';
-	}
-	if typeof lastname === 'undefined') {
-		lastname = 'Tamm';
-}
-*/
-	return 'Tere, ' + firstname + ' ' + lastname
-}
+let val;
 
-//kasutamine
-greet = greeting('Anna', 'Karutina')
-console.log(greet)
+val = document;
+val = document.all;
+val = document.all[4];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
 
-/// f-on defineeritud nagu const
-const square = function(number = 3) {
-	return Math.pow(number, 2);
-};
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].method;
+val = document.forms[0].action;
+val = document.forms[0].id;
 
-console.log(square())
+val = document.links;
+val = document.links[1];
+val = document.links[1].id;
+val = document.links[1].className;
 
-//
-(function() {
-	console.log('function is ran');
-}();
-(function(name){
-	console.log('tere, ' + name)
-}) ('Anna');
+val = document.scripts;
+val = document.scripts[0];
+val = document.scripts[0].getAttribute('src');
 
-//f-oni objekti atribuutidena
-const todoList = {
-	add: function(task){
-		console.log('Task is added');
-	},
-	edit: function(oldtask, newtask){
-		console.log(`${oldtask} is changed to ${newtask}`)
-	},
-	delete: function(task) {
-		console.log(task + ' is deleted');
-	}
-};
-
-todoList.add('Study JS');
-todoList.edit('Study JS', 'Study TypeScript');
-todoList.delete('Study JS');
+console.log(val);
