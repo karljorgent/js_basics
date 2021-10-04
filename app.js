b02d9078bd9eb6f71026df0cbfb924ba0f97d9e4
karljorgent <karl-jorgen.tormet@khk.ee>
2021-10-04 13:55:49 +0300

    addtask functional

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 // event elements
-const form = document.querySelector('form');
-const taskInput = document.querySelector('#task');
-
-
-//keyboard evenets
-//taskInput.addEventListener('keydown', runEvent)
-//taskInput.addEventListener('keypress', runEvent)
-
-//focus & blur
-//taskInput.addEventListener('focus', runEvent)
-//taskInput.addEventListener('blur', runEvent)
-
-
-taskInput.addEventListener('cut', runEvent)
-taskInput.addEventListener('paste', runEvent)
-taskInput.addEventListener('input', runEvent)
-
+const list = document.querySelector('ul')
+list.addEventListener('click', runEvent)
 
 
 
 function runEvent(e) {
 	console.log(`Event is ${e.type}`);
 	//something to do
-	console.log(e.target.value)
 }
