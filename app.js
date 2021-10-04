ebe07d2baf5846c7899e4210fd79edd568b4cb7c
karljorgent <karl-jorgen.tormet@khk.ee>
2021-10-04 13:09:55 +0300

    elementide käsitlemine

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-let val;
-// elementide loomine
-const li = document.createElement('li');
-// klassi määramine
-li.className = 'collection-item';
-// teksi lisamine
-li.appendChild(document.createTextNode('Study JS element creation'));
+// event elements
+const form = document.querySelector('form');
+const taskInput = document.querySelector('#task');
 
-// kodune töö
-// loo link X näitamiseks
-// ja lisa li sisse
-// siin peab olema sinu kood
-const a = document.createElement('a');
-a.className = 'secondary-content';
-a-appendChild(document.createTextNode('X'))
-li.appendChild(a)
-
-// lisame li element ul-sse
-const ul = document.querySelector('ul');
-ul.appendChild(li);
+form.addEventListener('submit', runEvent)
+function runEvent(e) {
+	console.log(`Event is ${e.type}`);
+	// something to do
+	console.log(taskInput.value)
+	taskInput.value = ''
+	e.preventDefault()
+}
