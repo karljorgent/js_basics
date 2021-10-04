c5e334dae5594e17d969ada133f80a1750f27452
karljorgent <karl-jorgen.tormet@khk.ee>
2021-10-04 13:20:28 +0300

    keyboard events

--- app.js
+++ app.js
@@ -2,11 +2,25 @@
 const form = document.querySelector('form');
 const taskInput = document.querySelector('#task');
 
-form.addEventListener('submit', runEvent)
+
+//keyboard evenets
+//taskInput.addEventListener('keydown', runEvent)
+//taskInput.addEventListener('keypress', runEvent)
+
+//focus & blur
+//taskInput.addEventListener('focus', runEvent)
+//taskInput.addEventListener('blur', runEvent)
+
+
+taskInput.addEventListener('cut', runEvent)
+taskInput.addEventListener('paste', runEvent)
+taskInput.addEventListener('input', runEvent)
+
+
+
+
 function runEvent(e) {
 	console.log(`Event is ${e.type}`);
-	// something to do
-	console.log(taskInput.value)
-	taskInput.value = ''
-	e.preventDefault()
+	//something to do
+	console.log(e.target.value)
 }
