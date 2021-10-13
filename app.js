aade896763acfd1be560967268b0207fe34a6e52
karljorgent <karl-jorgen.tormet@khk.ee>
2021-10-13 15:29:52 +0300

    local storage

--- app.js
+++ app.js
@@ -1,10 +1,77 @@
 // event elements
-const list = document.querySelector('ul')
-list.addEventListener('click', runEvent)
+const form = document.querySelector('form');
+const taskInput = document.querySelector('#task');
+const list = document.querySelector('ul');
+const remove = document.querySelector('#remove-all')
+
+list.addEventListener('click', removeTask)
+form.addEventListener('submit', addTask);
+remove.addEventListener('click', removeAll)
+
+function addTask(e) {
+	const li = document.createElement('li');
+	
+	li.className = 'collection-item';
+	li.appendChild(document.createTextNode(taskInput.value));
+
+	const link = document.createElement('a');
+	link.className = 'secondary-content';
+	link.appendChild(document.createTextNode('X'));
+	link.setAttribute('href', '#');
+
+	li.appendChild(link);
+	list.appendChild(li);
+
+	addTaskToLS(taskInput.value)
+	
+
+	taskInput.value = '';
+
+	e.preventDefault();
+}
+
+list.addEventListener('click', removeTask)
 
 
+function removeTask(e) {
+	if(e.target.textContent == 'X'){
+		if(confirm('Are you sure about that?')) {
+			list.removeChild(e.target.parentNode);
 
-function runEvent(e) {
-	console.log(`Event is ${e.type}`);
-	//something to do
+			removeTaskFromLS(e.target.parentElement.firstChild.textContent)
+		}
+	}
+}
+
+function removeAll() { 
+	while (list.firstChild) {
+		list.removeChild(list.firstChild)
+	}
+	localStorage.clear()
+}
+
+function addTaskToLS(task) {
+	let tasks;
+	if(localStorage.getItem('tasks') === null){
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'))
+	}
+	tasks.push(task);
+	localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function removeTaskFromLS(task) {
+	let tasks;
+	if(localStorage.getItem('tasks') === null){
+		tasks = [];
+	} else {
+		tasks = JSON.parse(localStorage.getItem('tasks'))
+	}
+	tasks.forEach(function(tasksElement, tasksIndex){
+		if(tasksElement === task){
+			tasks.splice(tasksIndex, 1);
+		}
+	});
+	localStorage.setItem('tasks', JSON.stringify(tasks))
 }
